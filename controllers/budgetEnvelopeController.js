7dea0d446c27ec5edfb4c275bebf3e32dce7c990
lordTocho <brian.highnam@accenturefederaldefense.com>
2021-03-21 18:42:39 -0400

    working through the joins

--- controllers/budgetEnvelopeController.js
+++ controllers/budgetEnvelopeController.js
@@ -1,7 +1,17 @@
 const BudgetEnvelope = require('../models/').BudgetEnvelope
+const User = require('../models/').User
 
 module.exports = {
+    /*
+        SELECT *
+	        FROM public."BudgetEnvelopes"
+		    INNER JOIN public."Users" ON public."BudgetEnvelopes"."userId" = public."Users"."userId";
+    */
     list( req, res ) {
-        
+        // list out all of the envelopes that each user has in the system
+        return BudgetEnvelope
+            .findAll({ include: User, as: 'Users' })
+            .then( ( envelope ) => res.status( 200 ).send( envelope ) )
+            .catch((error) => res.status(400).send(error));
     }
 }
--- routes/budgetEnvelope.js
+++ routes/budgetEnvelope.js
@@ -2,3 +2,7 @@ var express = require('express');
 var budgetEnvelopeRouter = express.Router();
 
 var budgetEnvelopeController = require('../controllers/budgetEnvelopeController');
+
+budgetEnvelopeRouter.get('/', budgetEnvelopeController.list );
+
+module.exports = budgetEnvelopeRouter;
