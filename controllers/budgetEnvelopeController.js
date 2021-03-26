b4e13331ce66999a51e35d2dd9a8f547ad268d6f
lordTocho <brian.highnam@accenturefederaldefense.com>
2021-03-25 21:32:06 -0400

    finished with the logic! next is the documentation

--- controllers/budgetEnvelopeController.js
+++ controllers/budgetEnvelopeController.js
@@ -1,6 +1,8 @@
 const BudgetEnvelope = require('../models/').BudgetEnvelope
 const User = require('../models/').User
 
+// contains all of the logic that recieves the API calls and runs commands against the database
+
 module.exports = {
     /*
         SELECT *
@@ -18,11 +20,95 @@ module.exports = {
         // list out all of the envelopes that each user has in the system
         return BudgetEnvelope
             .findAll({ include: [{
-                model: User,
-                required: true
-               }]
+                                    model: User,
+                                    required: true
+                                }],
             })
             .then( ( envelope ) => res.status( 200 ).send( envelope ) )
             .catch((error) => res.status(400).send(error));
+    },
+
+    /*
+        SELECT "BudgetEnvelope"."BudgetEnvelopeId", "BudgetEnvelope"."EnvelopeDescription", "BudgetEnvelope"."Budget", "BudgetEnvelope"."userId", "BudgetEnvelope"."createdAt", "BudgetEnvelope"."updatedAt", "User"."userId" AS "User.userId", "User"."firstName" AS "User.firstName", "User"."lastName" AS "User.lastName", "User"."createdAt" AS "User.createdAt", "User"."updatedAt" AS "User.updatedAt" FROM "BudgetEnvelopes" AS "BudgetEnvelope" INNER JOIN "Users" AS "User" ON "BudgetEnvelope"."userId" = "User"."userId" WHERE "BudgetEnvelope"."BudgetEnvelopeId" = '1';
+    */
+    getEnvelopeById( req, res ) {
+        // fetch budget envelope based on ID
+        return BudgetEnvelope
+            .findByPk(req.params.envelopeId,{
+                include: [{
+                    model: User,
+                    required: true
+                }],
+
+            })
+            .then((envelope) => {
+            if (!envelope) {
+              return res.status(404).send({
+                message: 'Envelope was not found',
+              });
+            }
+            return res.status(200).send({ envelopes: envelope,
+                                          response: "Budget envelopes sent successfully"
+            });
+          })
+          .catch((error) => res.status(400).send(error));
+
+    },
+
+    addEnvelopesWithUsers( req, res ){
+        return BudgetEnvelope
+            .create({
+                EnvelopeDescription: req.body.envelopeDescription,
+                Budget: req.body.budget,
+                userId: req.body.userId
+            })
+            .then((envelope) => res.status(201).send({ envelope: envelope,
+                                                       response: "New envelope record created successfully"}))
+            .catch((error) => res.status(400).send(error));
+    },
+
+    updateBudgetEnvelope( req, res ) {
+
+        let envelopeDescription = req.body.envelopeDescription
+        let budget = req.body.budget
+
+        return BudgetEnvelope
+            .findByPk(req.params.envelopeId)
+            .then( ( envelope ) => {
+
+                if ( !envelope ) {
+                    return res.status(404).send({
+                      message: 'Envelope Not Found',
+                    });
+                }
+                return envelope
+                    .update({
+                        EnvelopeDescription: envelopeDescription,
+                        Budget: budget
+                    })
+                    .then(() => res.status(200).send( { updateBudgetEnvelope: envelope,
+                                                        response: 'Envelope Update successful'
+                     } ) )
+                    .catch((error) => res.status(400).send(error));
+            } )
+    },
+
+    // deletes records
+    delete( req, res ) {
+        return BudgetEnvelope
+                .findByPk(req.params.envelopeId)
+                .then( ( envelope ) => {
+
+                    if ( !envelope ) {
+                        return res.status(404).send({
+                          message: 'Budget Envelope Not Found',
+                        });
+                    }
+                    return envelope
+                        .destroy()
+                        .then(() => res.status(200).send( { response: "Envelope successfully deleted." } ) )
+                        .catch((error) => res.status(400).send(error));
+                } )
+                .catch((error) => res.status(400).send(error));
     }
 }
--- routes/budgetEnvelope.js
+++ routes/budgetEnvelope.js
@@ -4,5 +4,12 @@ var budgetEnvelopeRouter = express.Router();
 var budgetEnvelopeController = require('../controllers/budgetEnvelopeController');
 
 budgetEnvelopeRouter.get('/', budgetEnvelopeController.list );
+budgetEnvelopeRouter.get('/:envelopeId', budgetEnvelopeController.getEnvelopeById );
+
+budgetEnvelopeRouter.post('/add', budgetEnvelopeController.addEnvelopesWithUsers );
+
+budgetEnvelopeRouter.put('/:envelopeId', budgetEnvelopeController.updateBudgetEnvelope )
+
+budgetEnvelopeRouter.delete('/:envelopeId', budgetEnvelopeController.delete )
 
 module.exports = budgetEnvelopeRouter;
